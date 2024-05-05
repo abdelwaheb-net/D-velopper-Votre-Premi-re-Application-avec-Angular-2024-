bc000ed503926c65195eedbd4d89df2135da8712
ANETI-Tunnisia <abdelwaheb_ammar@aneti.tn>
2024-05-05 01:21:16 +0100

    add pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { POKEMONS } from "./mock-pokemons";
+import { Pokemon } from './pokemon';
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
 
-    <router-outlet />
-  `,
-  styles: [],
+  templateUrl: 'app.component.html',
+  // styles: [],
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+  pokemonsList:Pokemon[] = POKEMONS;
+  pokemonSelected:Pokemon|undefined;
+  ngOnInit() {
+    console.table(this.pokemonsList);
+    // this.selectPokemon(this.pokemonsList[1]);
+  }
+/*   selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  } */
+/*   selectPokemon(event:MouseEvent) {
+    const index:number=+(event.target as HTMLInputElement).value;
+    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonsList[index].name}`);
+  } */
+  selectPokemon(pokemonId:string) {
+  //  const  id = +pokemonId;
+   const pokemon:Pokemon|undefined=this.pokemonsList.find(pokemon=>pokemon.id== +pokemonId);
+   if(pokemon){
+    console.log(`Vous avez démandé le pokémon ${pokemon.name}`);
+    this.pokemonSelected=pokemon;
+  }
+  else
+  {
+    console.log(`Vous avez demandé un pokemon qui n'existe pas` );
+    this.pokemonSelected=pokemon;
+  }
+  }
 }
