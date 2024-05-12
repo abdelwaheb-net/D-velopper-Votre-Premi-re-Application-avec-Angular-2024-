c38b3019abe1c077e98fc44aba100c43f3579b00
abdelwaheb-net <abdelwaheb_ammar@hotmail.fr>
2024-05-12 17:22:08 +0100

    Section7/Prendre en compte les actions de l’utilisateur

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 //for test1
 @Directive({
   selector: '[pkmnBorderCard]',
@@ -9,6 +9,12 @@ export class BorderCardDirective {
   constructor(private el:ElementRef) {
     this.setHeight(180);
     this.setBorder('#f5f5f5');
+   }
+   @HostListener('mouseenter') onMouseEnter(){
+    this.setBorder('#009688');
+   }
+   @HostListener('mouseleave') onMouseLeave(){
+    this.setBorder("#f5f5f5");
    }
 private setHeight(height:number) {
   this.el.nativeElement.style.height=`${height}px`;  
