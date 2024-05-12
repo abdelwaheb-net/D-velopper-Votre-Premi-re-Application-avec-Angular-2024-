0f727590f2135cbeecdd9bd6a753c36416d90e87
ANETI-Tunnisia <abdelwaheb_ammar@aneti.tn>
2024-05-12 16:59:22 +0100

    Section7/Créer une directive d’attribut

---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[pkmnBorderCard]',
+  standalone: true
+})
+export class BorderCardDirective {
+
+  constructor(private el:ElementRef) {
+    this.setHeight(180);
+    this.setBorder('#f5f5f5');
+   }
+private setHeight(height:number) {
+  this.el.nativeElement.style.height=`${height}px`;  
+}
+private setBorder(color:string) {
+  this.el.nativeElement.style.border='solid 4x ${color}';
+  
+}
+}
